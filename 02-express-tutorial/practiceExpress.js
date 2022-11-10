1976d478590dfd0139919f0dd6c189169c5fd095
dianayossi <dycarachure@gmail.com>
2022-11-09 21:25:13 -0800

    answers for lesson 4

--- 02-express-tutorial/practiceExpress.js
+++ 02-express-tutorial/practiceExpress.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const consoleLog = require('./practice-middleware');
+
+
+app.use(consoleLog);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './new-public/index.html'))
 })
-
 app.get('/sample', (req, res) => {
     res.send('This is working.')
 })
